e85e4f14585b5d763d6fa44b8e7fe044cb4c11ff
Sorin Duta <sorind@microsoft.com>
2024-08-02 15:29:16 +0300

    Default project and two endpoints.

---
+++ RateLimiter/RateLimiter/Controllers/Limited.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimiter.Controllers
+{
+    public class Limited : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet(nameof(Limited))]
+        public string Get()
+        {
+            return "Limited!";
+        }
+    }
+}
