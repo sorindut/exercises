f0c98a991b831a9128e42af3d849150e2f1ccee9
Sorin Duta <sorind@microsoft.com>
2024-08-02 15:29:16 +0300

    Default project and two endpoints.

---
+++ RateLimiter/RateLimiter/Controllers/Unlimited.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RateLimiter.Controllers
+{
+    public class Unlimited : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet(nameof(Unlimited))]
+        public string Get()
+        {
+            return "Unlimited!";
+        }
+    }
+}
