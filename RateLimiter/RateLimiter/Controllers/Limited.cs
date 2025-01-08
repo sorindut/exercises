8c00962901a770ddd8e12d1654b41bd371e9e3fe
Sorin Duta <sorind@microsoft.com>
2025-01-08 15:01:07 +0200

    TokeBucket skeleton

--- RateLimiter/RateLimiter/Controllers/Limited.cs
+++ RateLimiter/RateLimiter/Controllers/Limited.cs
@@ -1,18 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RateLimiter.Utilities;
+using System.Net;
 
 namespace RateLimiter.Controllers
 {
     public class Limited : Controller
     {
+        ClientTokenBucket bucket;
+
+        public Limited(ClientTokenBucket bucket)
+        {
+            this.bucket = bucket;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
         [HttpGet(nameof(Limited))]
-        public string Get()
+        public IActionResult Get()
         {
-            return "Limited!";
+            var ipAddr = this.Request.HttpContext.Connection.RemoteIpAddress;
+            if (this.bucket.ValidateClient(ipAddr!))
+            {
+                return Ok();
+            }
+            else
+            {
+                return new StatusCodeResult(429);
+            }
         }
     }
 }
--- RateLimiter/RateLimiter/Program.cs
+++ RateLimiter/RateLimiter/Program.cs
@@ -1,7 +1,9 @@
+using RateLimiter.Utilities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<ClientTokenBucket>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
