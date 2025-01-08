8b4a5be53192b46dd089fc74e599ee528d337757
Sorin Duta <sorind@microsoft.com>
2025-01-08 15:01:07 +0200

    TokeBucket skeleton

---
+++ RateLimiter/RateLimiter/Utilities/ClientTokenBucket.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using System.Timers;
+
+namespace RateLimiter.Utilities
+{
+    public class ClientTokenBucket: IDisposable
+    {
+        private const byte MaxTokens = 10;
+
+        private System.Timers.Timer heartBeat;
+        private bool disposedValue = false;
+
+        Dictionary <IPAddress, int> tokenList = new Dictionary <IPAddress, int>();
+
+        public ClientTokenBucket()
+        {
+            heartBeat = new System.Timers.Timer(1000);
+            heartBeat.Elapsed += OnTimerExpired;
+            heartBeat.AutoReset = true;
+            heartBeat.Enabled = true;
+        }
+
+        public bool ValidateClient(IPAddress address)
+        {
+            Console.Write($"Validating {address}: ");
+            if (tokenList.ContainsKey(address))
+            {
+                Console.WriteLine($"Found {tokenList[address]} tokens");
+                bool valid = tokenList[address] > 0;
+                if (valid) 
+                    tokenList[address]--;
+                return valid;
+            }
+            else
+            {
+                Console.WriteLine("Not found. Added.");
+                tokenList.Add(address, 9);
+                return true;
+            }
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    heartBeat.Stop();
+                    heartBeat.Dispose();
+                }
+
+                this.disposedValue = true;
+            }
+        }
+
+        private void OnTimerExpired(object? sender, ElapsedEventArgs e)
+        {
+            Console.Write($"Timer Expired at {DateTime.Now}. Clients: ");
+            foreach (var token in tokenList)
+            {
+                Console.Write($"{token.Key} ");
+                if (token.Value >= MaxTokens)
+                {
+                    tokenList.Remove(token.Key);
+                }
+                else
+                {
+                    tokenList[token.Key]++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
